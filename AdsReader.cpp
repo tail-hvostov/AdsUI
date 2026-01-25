c47aac942768c6bf3f7d572408009155163f4cfe
Vitali Zhuravski <vitaliycof@outlook.com>
2026-01-25 23:16:02 +0300

    Реализовал функцию retrieve.

--- AdsReader.cpp
+++ AdsReader.cpp
@@ -1,9 +1,11 @@
 #include "AdsReader.hpp"
 
 #include <stdio.h>
+#include <atomic>
 
 namespace {
 	ADSR::AdsStats global_stats;
+	std::atomic_flag access_flag;
 }
 
 namespace ADSR {
@@ -16,6 +18,12 @@ namespace ADSR {
 	}
 
 	void retrieve(AdsStats& stats) {
-		puts("retrieve");
+		while (access_flag.test_and_set());
+		stats = global_stats;
+		for (int i = 0; i < INPUT_COUNT; i++) {
+			stats.average_v[i] /= stats.measurements;
+		}
+		std::memset(&global_stats, 0, sizeof(global_stats));
+		access_flag.clear();
 	}
 }
